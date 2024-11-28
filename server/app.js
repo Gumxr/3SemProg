7978ae6b0c632d1fb2586f17d01e8e31de628522
yahya637 <Alatteeah32@hotmail.com>
2024-11-28 15:10:30 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,45 +1,12 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// Create a new SQLite database
-let db = new sqlite3.Database(':memory:', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-});
-
-// Create a simple table
-db.serialize(() => {
-    db.run("CREATE TABLE user (id INT, name TEXT)");
-    db.run("INSERT INTO user (id, name) VALUES (1, 'John Doe')");
-});
-
-// Define a route
-app.get('/', (req, res) => {
-    db.all("SELECT * FROM user", [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
-
-// Close the database connection when the app is terminated
-process.on('SIGINT', () => {
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Closed the database connection.');
-        process.exit(0);
-    });
 });
