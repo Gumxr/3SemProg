ad5095219506a4d73817ecd309749a53414b4c5d
yahya637 <Alatteeah32@hotmail.com>
2024-11-28 15:07:47 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
